ee268301075ede14615c1a7973c3d6c6eaf4c69c
Harsha <saisri2946@gmail.com>
2024-12-20 23:07:20 +0530

    Checkpoint (test for passing state as prop)

--- ChefClaudeApp/src/components/Form.jsx
+++ ChefClaudeApp/src/components/Form.jsx
@@ -1,27 +1,64 @@
 import { useState } from "react";
 import IngredientList from "./IngredientList";
-import GetRecipie from "./GetRecipie";
+// import GetRecipie from "./GetRecipie";
+import RecipieDetails from "./RecipieDetails";
 
 export default function Form() {
-    function isBlank(str) {
-        return !str || /^\s*$/.test(str);
+	function isBlank(str) {
+		return !str || /^\s*$/.test(str);
+	}
+	const [recipieShown, setRecipieShown] = useState(false);
+	const [ingredients, setIngredients] = useState([]);
+	const addIngredient = function (formData) {
+		!isBlank(formData.get("ingredient"))
+			? setIngredients((prevArray) => [
+					...prevArray,
+					formData.get("ingredient"),
+			  ])
+			: null;
+	};
+    const getRecipie = ()=>{
+        setRecipieShown(true);
     }
-    const [ingredients, setIngredients] = useState([]);
-    const addIngredient = function(formData) {
-        !isBlank(formData.get("ingredient"))?setIngredients((prevArray)=>[...prevArray, formData.get("ingredient")]):null;
-    }
-    return(
-            <>
-                <div className="input-container">
-                    <form action={addIngredient}>
-                        <input type="text" placeholder="eg. orange" aria-label="add ingredient" name="ingredient"/>
-                        <button className="add-ingredient">Add Ingredient</button>
-                    </form>
-                    <div className="text-container">
-                        {ingredients.length!=0?<IngredientList ingredients={ingredients.map(x=><li key={x} className="ingredient">{x}</li>)} />:null}
-                    </div>
-                    {ingredients.length>=3?<GetRecipie />:null}
-                </div>
-            </>
-    )
+	return (
+		<>
+			<div className="input-container">
+				<form action={addIngredient}>
+					<input
+						type="text"
+						placeholder="eg. orange"
+						aria-label="add ingredient"
+						name="ingredient"
+					/>
+					<button className="add-ingredient">Add Ingredient</button>
+				</form>
+				<div className="text-container">
+					{ingredients.length != 0 ? (
+						<IngredientList
+							ingredients={ingredients.map((x) => (
+								<li key={x} className="ingredient">
+									{x}
+								</li>
+							))}
+						/>
+					) : null}
+				</div>
+				{ingredients.length >= 3 ? (
+					<div className="recipie-getter-card">
+						<div className="recipie-text-container">
+							<h3>Ready for a recipie?</h3>
+							<span>
+								Generate a recipie from your list of
+								ingredients.
+							</span>
+						</div>
+						<div className="recipie-button-container">
+							<button type="button" onClick={getRecipie}>Get a recipie</button>
+						</div>
+					</div>
+				) : null}
+				{recipieShown ? <RecipieDetails /> : null}
+			</div>
+		</>
+	);
 }
---
+++ ChefClaudeApp/src/components/RecipieDetails.jsx
@@ -0,0 +1,7 @@
+export default function RecipieDetails(props) {
+    return(
+        <>
+            <h1>Placeholder</h1>
+        </>
+    )
+}
