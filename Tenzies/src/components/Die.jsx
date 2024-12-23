c5f5ed182806b3d802342b92a435e170c4666878
Harsha <69705177+ssaisriharsha@users.noreply.github.com>
2024-12-23 22:25:54 +0530

    Added dice holding functionality. Yet to implement die rolling

--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -1,13 +1,18 @@
 import PropTypes from "prop-types"
 
 export default function Die(props) {
+    const handleClick = () =>props.holdfn(props.id);
+
     return(
         <>
-            <div className="die"><b>{props.dieValue}</b></div>
+            <div className={props.held===true?"die held":"die"} onClick={handleClick}><b>{props.dieValue}</b></div>
         </>
     )
 }
 
 Die.propTypes = {
     dieValue: PropTypes.number.isRequired,
+    held: PropTypes.bool.isRequired,
+    holdfn: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
 }
--- Tenzies/src/components/GameCard.jsx
+++ Tenzies/src/components/GameCard.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react"
 import Die from "./Die"
+import { nanoid } from "nanoid";
 
 export default function GameCard() {
+    function generateRandomArrayObject() {
+        const arr = [];
+        for(let i =  0; i < 10; i++) {
+            arr.push(
+                {
+                    value: Math.floor(Math.random()*6)+1,
+                    isHeld: false,
+                    id: nanoid(),
+            }
+        ); 
+        }
+        return arr;
+    }
+
+    const rollDice = ()=>{setDieValueArray(generateRandomArrayObject())}
+    const hold  = (id)=>{setDieValueArray(prevArray=>prevArray.map(x=>x.id===id?{...x, isHeld: !x.isHeld}:{...x}))};
+
+    const [dieValueArray, setDieValueArray] = useState(generateRandomArrayObject());
+    let dieComponents = dieValueArray.map((x)=><Die key={x.id} dieValue={x.value} held={x.isHeld} id={x.id} holdfn={hold}/>)
     return (
         <div className="game-card">
             <h1>Tenzies</h1>
             <p>Roll until all dice are the same. Click each die to freeze at its current value between rolls.</p>
             <div className="dice-container">
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
-                <Die dieValue={5} />
+                {dieComponents}
             </div>
             <div className="btn-container">
-                <button className="roll">Roll dice</button>
+                <button className="roll" onClick={rollDice}>Roll dice</button>
             </div>
         </div>
     )
